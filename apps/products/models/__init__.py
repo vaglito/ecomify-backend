f47fcce86ac20c1e085324c3be49824462f7d267
Cesar Gonzalez <cesargonzalez390@gmail.com>
2026-02-01 14:24:44 -0500

    feat: Implementación inicial de la aplicación `products` con `API viewsets`, `serializers`, configuraciones de `admin` y estructura de `modelos`.

---
+++ apps/products/models/__init__.py
@@ -0,0 +1,4 @@
+from .categories import Category, SubCategory
+from .brands import Brand
+from .products import Product, ProductImage, Specification, product_image_upload_path
+from .statistics import ProductStatistics
